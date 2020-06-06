74682069865b99c5be6ac4308b610ab57af89ad5
daryademkova <>
2020-06-06 23:28:41 +0300

    Доработка

--- src/js/modules/ws.js
+++ src/js/modules/ws.js
@@ -1,23 +1,13 @@
-function createWS(url, payloads){
-    console.log(url)
+function createWS(url, payloads) {
+    console.log(url);
     var socket = new WebSocket(url);
-      
-    socket.onclose = function(event) {
-        if (event.wasClean) {
-            alert('Соединение закрыто чисто');
-        } else {
-            alert('Обрыв соединения');
-        }
 
-        alert('Код: ' + event.code + ' причина: ' + event.reason);
-    };
-      
     socket.onmessage = function(event) {
         var message = JSON.parse(event.data);
 
-        payloads[message.payload](message.data, socket)
+        payloads[message.payload](message.data, socket);
     };
-      
+
     socket.onerror = function(error) {
         alert("Ошибка " + error.message);
     };
@@ -27,4 +17,4 @@ function createWS(url, payloads){
 
 export {
     createWS
-}
+};
